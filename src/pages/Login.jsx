4e1ae938028083d2d4607a9d9287532c43ca9b5b
Peter Tung <pktung470@gmail.com>
2025-11-24 12:00:45 +0530

    update3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,10 @@ const Login = () => {
 
   const [currentState, setCurrentState] = useState('Log In');
 
+  const onSubmitHandle = (event)=>{
+   event.preventDefault();
+  }
+
   return (
     <div className='w-[95%] mx-auto'>
       <motion.div 
@@ -16,37 +20,37 @@ const Login = () => {
          }}
       className='w-[300px] sm:w-[600px] px-6 py-4 mx-auto mb-9 '>
 
-        <form
+        <form onSubmit={onSubmitHandle}
         className='flex flex-col gap-2'>
           <p className='flex font-bold py-4 text-2xl items-center justify-center'>{currentState} </p>
 
           {currentState === 'Log In' ? '' : <div className='flex flex-col gap-2'>
             <label className='text-xl font-semibold' htmlFor='name'> Name</label>
-            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='text' id='name' placeholder='Enter your name'/>
+            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='text' id='name' placeholder='Enter your name' required/>
           </div>
             }
 
            <div className='flex flex-col gap-2'>
             <label className='text-xl font-semibold' htmlFor='email'> Email</label>
-            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='email' id='email' placeholder='e.g. name@gmail.com'/>
+            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='email' id='email' placeholder='e.g. name@gmail.com' required/>
           </div>
 
           {currentState === 'Log In' ? '' :
            <div className='flex flex-col gap-2'>
             <label className='text-xl font-semibold' htmlFor='phone'> Phone</label>
-            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='number' id='phone' placeholder='Enter your phone number'/>
+            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='number' id='phone' placeholder='Enter your phone number' required/>
           </div>
             }
 
           <div className='flex flex-col gap-2'>
             <label className='text-xl font-semibold' htmlFor='password'> Password</label>
-            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='password' id='passowrd' placeholder='Enter your password'/>
+            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='password' id='passowrd' placeholder='Enter your password' required/>
           </div>
 
           {currentState === 'Log In' ? '' :
           <div className='flex flex-col gap-2'>
             <label className='text-xl font-semibold' htmlFor='confirmpassword'> Confirm Password</label>
-            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='password' id='confirmpassword' placeholder='Confirm your password'/>
+            <input className='py-3 px-4 border-2 border-amber-500 rounded-lg' type='password' id='confirmpassword' placeholder='Confirm your password' required/>
           </div>
           }
 
@@ -57,8 +61,8 @@ const Login = () => {
            </div> : <button onClick={()=>setCurrentState('Log In')} className='flex justify-end cursor-pointer text-red-800 hover:text-amber-600 active:text-amber-700 '>login</button> 
            }
 
-          {currentState === 'Log In'? <button className='w-full py-3 px-4 mt-3 bg-amber-500 rounded-lg font-bold text-xl text-white hover:bg-amber-600 active:bg-amber-800'>Log In </button> 
-          : <button className='w-full py-3 px-4 mt-3 bg-amber-500 rounded-lg font-bold text-xl text-white hover:bg-amber-600 active:bg-amber-800'>Sign Up</button> }
+          {currentState === 'Log In'? <button type='submit' className='w-full py-3 px-4 mt-3 bg-amber-500 rounded-lg font-bold text-xl text-white hover:bg-amber-600 active:bg-amber-800'>Log In </button> 
+          : <button type='submit' className='w-full py-3 px-4 mt-3 bg-amber-500 rounded-lg font-bold text-xl text-white hover:bg-amber-600 active:bg-amber-800'>Sign Up</button> }
 
         </form>
       </motion.div>
